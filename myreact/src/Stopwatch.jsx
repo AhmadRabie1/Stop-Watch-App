dfa9c09d937e7533e36b3aec2e5d14cba39aed12
Ahmad <rabieahmad330@gmail.com>
2024-09-02 14:45:08 +0300

    added all the button and finished all html

--- myreact/src/Stopwatch.jsx
+++ myreact/src/Stopwatch.jsx
@@ -1,37 +1,44 @@
 import { useEffect, useRef, useState } from "react";
 
 
-function Stopwatch(){
+function Stopwatch() {
 
     const [isRunning, setIsRunning] = useState(false);
     const [elapsedTime, setElapsedTime] = useState(0);
     const intervalIdRef = useRef(null);
-    const startTimeRef  = useRef(0);
+    const startTimeRef = useRef(0);
 
     useEffect(() => {
 
     }, [isRunning]);
 
-    function start(){
+    function start() {
 
     }
 
-    function stop(){
+    function stop() {
 
     }
 
-    function reset(){
-
-    }
-    
-    function formatTime(){
+    function reset() {
 
     }
 
-    return(
+    function formatTime() {
+
+        return `00:00:00`;
+    }
+
+    return (
         <div className="stopwatch">
-            <div className="display">{formatTime()}</div>
-            
+            <div className="display">
+                {formatTime()}
+            </div>
+            <div className="controls">
+                <button onClick={start} className="start-button">Start</button>
+                <button onClick={stop} className="stop-button">Stop</button>
+                <button onClick={reset} className="reset-button">Reset</button>
+            </div>
         </div>
     );
 }
